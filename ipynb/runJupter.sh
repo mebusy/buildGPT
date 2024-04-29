c9cb1a87433347de21b065e569d810f485533289
mebusy <qibinyi@mebusys-iMac>
2024-04-29 13:05:56 +0800

    + runJutper.sh

---
+++ ipynb/runJupter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+docker run --rm -it -p 8888:8888 \
+        -v "$(pwd)":/workspace --pids-limit 16384 \
+        mebusy/pytorch bash -c "cd /workspace && jupyter notebook --allow-root --ip 0.0.0.0 --no-browser"
