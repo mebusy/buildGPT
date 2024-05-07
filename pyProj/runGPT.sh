ee46a8d2f4a189fe0d658ee6050671ceba31ca0a
mebusy <qibinyi@mebusys-iMac>
2024-05-07 18:39:46 +0800

    m

--- pyProj/runGPT.sh
+++ pyProj/runGPT.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-docker run --rm -it -v "$(pwd)":/workspace  mebusy/pytorch bash -c "cd /workspace && python gpt.py"
+
+# if os is darwin
+if [ "$(uname)" == "Darwin" ]; then
+    docker run --rm -it -v "$(pwd)":/workspace  mebusy/pytorch bash -c "cd /workspace && python gpt.py"
+else
+    docker run  --rm -it -v `pwd`:/opt/work/  --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864  mebusy/nv_torch_gym  python gpt.py
+fi
+
